6b26c6f4acf742b3603d4c768c55b4921c0a1e4a
LMikeH <lmichaelhuang3@gmail.com>
2024-09-22 11:44:22 -0400

    cleaned out stock photos from template and updated heroku container script

--- heroku_deploy_app.zsh
+++ heroku_deploy_app.zsh
@@ -1,4 +1,4 @@
-
+heroku container:login
 docker build -f Dockerfile.web --platform linux/amd64 -t registry.heroku.com/thundergraph-landing-page/web .
 docker push registry.heroku.com/thundergraph-landing-page/web
 heroku container:release web -a thundergraph-landing-page
